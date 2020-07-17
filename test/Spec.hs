a3a081ee71ae2439b86230635e94b9fea064551e
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-17 19:43:45 +0000

    Add initial structure
    Change-Id: Iad865f94ea6ba465707922c2b53a9ad36a082af7

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,31 @@
 module Main (main) where
 
+import Data.Aeson (decode)
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as BSL
+import Data.Maybe (isJust)
+import Test.Tasty
+import Test.Tasty.HUnit
+import Zuul
+import Zuul.Status as Zuul
 
 main :: IO ()
-main = putStrLn ("Test suite is not implemented" :: String)
+main = do
+  dataFile <- BSL.readFile "./test/data/Status.json"
+  defaultMain (tests dataFile)
+
+tests :: ByteString -> TestTree
+tests dataFile = testGroup "Tests" [encodingTests dataFile]
+
+encodingTests :: ByteString -> TestTree
+encodingTests dataFile =
+  testGroup
+    "FromJSON"
+    [ testCase "Test Status.json"
+        $ assertBool "ZuulSTatus is decoded"
+        $ isStatus (decode dataFile)
+    ]
+  where
+    isStatus :: Maybe Zuul.Status -> Bool
+    isStatus (Just _) = True
+    isStatus Nothing = False
